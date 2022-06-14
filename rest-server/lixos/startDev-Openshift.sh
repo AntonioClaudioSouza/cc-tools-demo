7aecf9183bf4c598bd1f2d69824260778818a82f
Cloud User <centos@ip-172-31-10-22.ec2.internal>
2022-06-14 00:04:15 +0000

    script para openshit com acesso aos volumes, graças a Deus

---
+++ rest-server/lixos/startDev-Openshift.sh
@@ -0,0 +1,10 @@
+#oc delete -f podssh.yaml
+#sleep 5
+#oc create -f podssh.yaml
+#sleep 5
+
+oc rsync ./ ssh-service-pod:/tmp/rest-server/ -c ssh-service-img
+oc rsync ../fabric/crypto-config/rest-certs/ ssh-service-pod:/tmp/certs/ -c ssh-service-img
+
+#oc rsync rest-server/ ssh-service-pod:/tmp/rest-server/ -c ssh-service-img
+#oc rsync fabric/crypto-config/rest-certs/ ssh-service-pod:/tmp/certs/ -c ssh-service-img
